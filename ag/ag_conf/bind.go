776d869704ce30f4df2f69c0c8bc55b9a6e0b8fe
sirius <916108538@qq.com>
2025-07-21 15:49:10 +0800

    ag_config watch update

--- ag/ag_conf/bind.go
+++ ag/ag_conf/bind.go
@@ -113,9 +113,8 @@ func (cpb *ConfigurationPropertiesBinder) Bind(i any, name ...string) error {
 
 // BindValue 绑定值
 func (cpb *ConfigurationPropertiesBinder) BindValue(bctx context.Context, v reflect.Value, param BindParam) (rterr error) {
-
 	// 默认所有Binder对象都自动刷新 TODO 刷新续精确控制，添加autorefresh标签功能
-	if bctx.Value(KEY_BindWatched) == nil {
+	if bctx.Value(KEY_BindWatched) == nil && v.Kind() != reflect.Pointer {
 		bctx = context.WithValue(bctx, KEY_BindWatched, true)
 		WatcherM.RegChangeListener(param.Key, func(ck, cv string) {
 			rbctx := context.WithValue(context.Background(), KEY_BindWatched, true)
