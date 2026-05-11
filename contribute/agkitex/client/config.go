cf6b9a64816586876287f9a2041872aed4a3a4e6
sirius <916108538@qq.com>
2026-05-11 14:31:15 +0800

    agkitex client default配置方式调整

--- contribute/agkitex/client/config.go
+++ contribute/agkitex/client/config.go
@@ -3,6 +3,7 @@ package client
 import (
 	"ag-core/ag/ag_conf"
 	"log/slog"
+	"runtime"
 	"time"
 )
 
@@ -14,10 +15,10 @@ const (
 // KitexClientConfig 客户端配置
 type KitexClientConfig struct {
 	// RPC超时时间（秒）
-	RpcTimeout time.Duration `value:"${:30}"`
+	RpcTimeout time.Duration
 
 	// 传输协议类型
-	TransportType string `value:"${:grpc}"`
+	TransportType string
 
 	// 服务发现配置
 	Resolver ResolverConfig
@@ -29,36 +30,58 @@ type KitexClientConfig struct {
 // ResolverConfig 服务发现配置
 type ResolverConfig struct {
 	// 是否启用服务发现
-	Enable bool `value:"${:false}"`
+	Enable bool
 
 	// 服务发现类型
-	Type string `value:"${type:agnacos}"`
+	Type string
 
 	// Nacos配置
-	Nacos NacosConfig `value:"${Nacos}"`
+	Nacos NacosConfig
 }
 
 type ConnConfig struct {
 	// GRPC连接池大小
-	GRPCConnPoolSize uint32 `value:"${:1}"`
+	GRPCConnPoolSize uint32
 }
 
 // NacosConfig Nacos配置
 type NacosConfig struct {
-	Group   string `value:"${group:DEFAULT_GROUP}"`
-	Cluster string `value:"${cluster:DEFAULT}"`
+	Group   string
+	Cluster string
 }
 
 // GrpcConfig GRPC配置
-type GrpcConfig struct {
-	Enable            bool `value:"${:false}"`
-	MaxConnectionIdle int  `value:"${:0}"`
-}
+// type GrpcConfig struct {
+// 	Enable            bool `value:"${:false}"`
+// 	MaxConnectionIdle int  `value:"${:0}"`
+// }
 
 // InitKitexClientConfig 初始化客户端配置
 func InitKitexClientConfig(binder ag_conf.IBinder) *KitexClientConfig {
-	config := &KitexClientConfig{}
+	config := defaultKitexClientConfig()
 	binder.Bind(config, KitexClientPropertiesPrefix)
 	slog.Debug("KitexClientConfig initialized", slog.Any("config", config))
 	return config
+}
+
+func defaultKitexClientConfig() *KitexClientConfig {
+
+	numP := runtime.GOMAXPROCS(0)
+	connPoolSize := uint32(numP * 3 / 2)
+
+	return &KitexClientConfig{
+		RpcTimeout:    30,
+		TransportType: "grpc",
+		Resolver: ResolverConfig{
+			Enable: true,
+			Type:   "agnacos",
+			Nacos: NacosConfig{
+				Group:   "DEFAULT_GROUP",
+				Cluster: "DEFAULT",
+			},
+		},
+		Conn: ConnConfig{
+			GRPCConnPoolSize: connPoolSize,
+		},
+	}
 }
